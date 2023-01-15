8ec9fe92743747d27ec5701f9c2abf1e7c78ba3a
leoantony72 <leoantony102@gmail.com>
2023-01-15 18:50:06 +0530

    🍥 Return Follower/Following count

--- auth/internal/repositories/get_user_by_id.go
+++ auth/internal/repositories/get_user_by_id.go
@@ -5,7 +5,7 @@ import "github.com/leoantony72/twitter-backend/auth/internal/model"
 func (u *UserPostgresRepo) GetUserbyId(id string) (*model.User, error) {
 	user := model.User{}
 	// result := u.db.Find(&user, id)
-	result := u.db.Model(&user).Select("id", "username", "date_created").Where("id =?", id).Scan(&user)
+	result := u.db.Model(&user).Select("id", "username", "date_created", "followers", "following").Where("id =?", id).Scan(&user)
 	if result.Error != nil {
 		return nil, result.Error
 	}
