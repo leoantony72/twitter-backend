eb2055599d9399a1861127484f76d1487be079af
leoantony72 <leoantony102@gmail.com>
2023-01-24 14:54:28 +0530

    fix:Get tweet by user

--- tweet/internal/repositories/repo_get_tweet_by_username.go
+++ tweet/internal/repositories/repo_get_tweet_by_username.go
@@ -3,7 +3,10 @@ package repositories
 import "github.com/leoantony72/twitter-backend/tweet/internal/model"
 
 func (t *TweetRepo) GetTweetByUser(username string) ([]*model.Tweets, error) {
-	tweet := model.Tweets{}
-	t.db.Model(&tweet).Select("id", "username", "created_at", "like_cout", "retweet_count").Where("username=?", username)
+	tweet := []model.Tweets{}
+	result := t.db.Model(&tweet).Select("id", "username", "created_at", "like_cout", "retweet_count").Where("username=?", username).Scan(&tweet)
+	if result.Error != nil {
+		return nil, result.Error
+	}
 	return nil, nil
 }
--- tweet/internal/services/get_tweet_by_username.go
+++ tweet/internal/services/get_tweet_by_username.go
@@ -3,5 +3,9 @@ package services
 import "github.com/leoantony72/twitter-backend/tweet/internal/model"
 
 func (t *TweetService) GetTweetByUser(username string) ([]*model.Tweets, error) {
-	return nil, nil
+	tweets, err := t.repo.GetTweetByUser(username)
+	if err != nil {
+		return nil, err
+	}
+	return tweets, nil
 }
